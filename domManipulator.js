16b7427aaf171b9b413b37c1e0535501cdbf56ae
khalidAYADI75 <khalidayadi75@gmail.com>
2022-07-28 21:24:07 +0200

    Implementation : When the user click on the bookmark icon V1.29

--- domManipulator.js
+++ domManipulator.js
@@ -1,15 +1,15 @@
 class domManipulator {
 
     static createBookElement(parent,book) {
-        parent.appendChild(this.componentCreation('div','Titre',book.title));
-        parent.appendChild(this.componentCreation('div','Id',book.id));
-        parent.appendChild(this.componentCreation('div','Description',book.description));
+        parent.appendChild(this.componentCreation('h3','Titre',book.title));
+        parent.appendChild(this.componentCreation('i','Id',book.id));
+        parent.appendChild(this.componentCreation('p','Description',book.description));
         parent.appendChild(this.componentCreation('img','',book.image));
     }
 
-    static componentCreation(divOrImg,entitled,elementbook) {
-        var creationDiv = document.createElement(divOrImg);
-        if (divOrImg=='img') {
+    static componentCreation(typeOfTag,entitled,elementbook) {
+        var creationDiv = document.createElement(typeOfTag);
+        if (typeOfTag=='img') {
             creationDiv.src=elementbook;
             creationDiv.setAttribute('class','image');
         } else {
